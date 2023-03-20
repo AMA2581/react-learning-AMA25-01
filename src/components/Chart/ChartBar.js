a8112453c85ae5bbf31f39f7c766d96eced6b286
AMA2581 <amir.abravesh.ama@gmail.com>
2023-03-20 11:53:07 +0330

    05 Rendering Lists & Conditional Content fix

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ChartBar.css";
 
-const Chartbar = (props) => {
+const ChartBar = (props) => {
   let barFillHeight = "0%";
 
   if (props.maxValue > 0) {
@@ -18,4 +18,4 @@ const Chartbar = (props) => {
   );
 };
 
-export default Chartbar;
+export default ChartBar;
